99edd2b87f0b6a6d59e6234921275efe0debc01c
Ruby Darku <rdarku147@gmail.com>
2023-06-23 11:21:27 -0400

    Initial Commit

---
+++ multipleViews/multipleViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  multipleViewsApp.swift
+//  multipleViews
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct multipleViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
